13f722497ecaea2e546ee2a9e16585083ef0147e
Eugene Ermakov <eugeniy696@gmail.com>
2026-09-21 16:22:42 +0300

    Testing dockerimage

--- infrastructure.sh
+++ infrastructure.sh
@@ -1,4 +1,8 @@
 helm upgrade --install gift-finder ./helm-charts/gift-finder --namespace gift-finder --create-namespace
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
-helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx --namespace ingress-nginx --create-namespace
+if ! helm status ingress-nginx -n ingress-nginx >/dev/null 2>&1; then
+    helm install ingress-nginx ingress-nginx/ingress-nginx \
+        --namespace ingress-nginx \
+        --create-namespace
+fi
