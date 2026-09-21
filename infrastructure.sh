573bbfa02ec959142a7703681bb3b3b16a56c252
Eugene Ermakov <eugeniy696@gmail.com>
2026-09-21 15:54:30 +0300

    Added k8s infrastructure for frontend, infrastructure.sh and fixed Dockerfile

---
+++ infrastructure.sh
@@ -0,0 +1,4 @@
+helm upgrade --install gift-finder ./helm-charts/gift-finder --namespace gift-finder --create-namespace
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx --namespace ingress-nginx --create-namespace
