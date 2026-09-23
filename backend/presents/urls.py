65785d58f8e0e2b38cab868f29d43bd9da0474ec
Viakol <viakol.lokaiv@gmail.com>
2026-09-23 08:20:28 +0300

    Add Map the Scraper Route (backend/presents/urls.py)

--- backend/presents/urls.py
+++ backend/presents/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from presents.views import product_gift_list, product_gift_detail
+from presents.views import product_gift_list, product_gift_detail, scrape_amazon_item
 
 
 urlpatterns = [
     path("api/gifts/", product_gift_list, name="gift-list"),
     path("api/gifts/<int:pk>/", product_gift_detail, name="gift-detail"),
+    path("api/gifts/scrape/", scrape_amazon_item, name="gift-scrape"),
 ]
