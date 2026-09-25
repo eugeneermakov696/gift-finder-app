361b72dc6a13e3fe0435d3e4c3fdccd5eb210ad0
Viakol <viakol.lokaiv@gmail.com>
2026-09-25 04:04:49 +0300

    Create the Serializers (backend/presents/serializers.py)

---
+++ backend/presents/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from presents.models import Present, Wishlist
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["id", "username", "password"]
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+
+class PresentSerializer(serializers.ModelSerializer):
+    price = serializers.FloatField()  # Cast Decimal fields cleanly for JSON output
+    original_price = serializers.FloatField(required=False, allow_null=True)
+    rating = serializers.FloatField(required=False, allow_null=True)
+
+    class Meta:
+        model = Present
+        fields = "__all__"
+
+
+class WishlistSerializer(serializers.ModelSerializer):
+    owner = serializers.CharField(source="user.username", read_only=True)
+    items = PresentSerializer(many=True, read_only=True)
+    items_count = serializers.IntegerField(source="items.count", read_only=True)
+
+    class Meta:
+        model = Wishlist
+        fields = ["id", "name", "owner", "items_count", "items"]
